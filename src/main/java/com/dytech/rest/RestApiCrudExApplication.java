662e28e22b51acd2684268ebb48ffaac7c1a8fde
Dyeggo Silva <dyeggo0102@live.com>
2023-04-30 06:16:06 -0300

    Initial
    CRUD com Java e Spring Boot,
    Commit projeto Spring inicial

---
+++ src/main/java/com/dytech/rest/RestApiCrudExApplication.java
@@ -0,0 +1,15 @@
+package com.dytech.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class RestApiCrudExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiCrudExApplication.class, args);
+	}
+
+}

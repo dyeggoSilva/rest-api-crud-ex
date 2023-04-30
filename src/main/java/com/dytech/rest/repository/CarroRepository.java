f6ba7c33c55a4ae41d945fd80a9a4a0ab4ed5c73
Dyeggo Silva <dyeggo0102@live.com>
2023-04-30 06:50:41 -0300

    Repository
    foi implementado uma interface como reporitório

---
+++ src/main/java/com/dytech/rest/repository/CarroRepository.java
@@ -0,0 +1,9 @@
+package com.dytech.rest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dytech.rest.carro.Carro;
+
+public interface CarroRepository extends JpaRepository<Carro, Long>{
+
+}
